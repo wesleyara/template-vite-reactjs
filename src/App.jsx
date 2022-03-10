45c1052350232ea09f83ba7fb2153252f7c4eda5
AugustoTI <augustocesarti2021@gmail.com>
2022-03-09 22:33:51 -0300

    template v1 --fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 function App() {
-  const [teste, setTeste] = React.useState(0);
-
-  React.useEffect(() => {
-    console.log(teste);
-  }, []);
-
-  return <h1>Oi</h1>;
+  return <h1>Hello</h1>;
 }
 
 export default App;
